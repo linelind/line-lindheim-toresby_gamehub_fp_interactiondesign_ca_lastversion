9b48ee20ec26946f226bdc96540e98dd0f5601ec
linelind <79406471+linelind@users.noreply.github.com>
2021-10-09 19:06:25 +0200

    added counter to cart

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const button = document.querySelector(".addToCartButton");
+const shoppingCart = document.querySelector(".shoppingCart");
+
+let count = 0;
+
+function cartCount() {
+    count++
+    shoppingCart.style.display = "block";
+    shoppingCart.innerHTML = count;
+    button.innerHTML = "Game added";
+    button.style.backgroundColor = "var(--successgreen-colour)";
+
+setTimeout(function() {
+    button.innerHTML = "Add to cart";
+    button.style.backgroundColor = "var(--darkorange-colour)";
+
+}, 1700);
+
+}
+
+button.addEventListener("mouseup", cartCount);
