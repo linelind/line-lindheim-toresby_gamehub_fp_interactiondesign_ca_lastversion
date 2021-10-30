515a39ee51bd4659068d009d7b4325a682b8f31e
linelind <79406471+linelind@users.noreply.github.com>
2021-10-30 14:21:07 +0200

    added games to shop from API

--- js/script.js
+++ js/script.js
@@ -21,4 +21,3 @@ function cartCount() {
 }
 
 button.addEventListener("mouseup", cartCount);
-
---
+++ js/shop.js
@@ -0,0 +1,47 @@
+
+const shopContainer = document.querySelector(".shopContainer");
+const url = "http://linelind.one/GameHubAPI/wp-json/wc/store/products";
+
+async function fetchGames() {
+
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+
+        console.log(json);
+
+        shopContainer.innerHTML = "";
+
+
+        json.forEach(function(game) {
+
+            const image = game.images;
+
+            for(let i = 0; i < image.length; i++) {
+                console.log(image[i].src);
+            }
+
+            shopContainer.innerHTML += `<a href="productdetails.html?id=${game.id}" class="gameCard">
+                                                <div class="image">
+                                                    <img src="${image[0].src}" alt="${image[0].alt}"/> 
+                                                </div>
+                                                <div class="details">
+                                                    <h2>${game.name}</h2>   
+                                                    <p>${game.price_html}</p> 
+                                                    <p class="cta_orange cta_viewGame">${game.add_to_cart.text}</p>                                                                                                                                                     
+                                                </div>
+                                        </a>`;
+        
+
+        }); 
+
+    }
+
+    catch(error) {
+        console.log(error);
+        shopContainer.innerHTML = message("error", error);
+    }
+
+}
+
+fetchGames();
