30191a398a85cb98287e516957102088dec31338
linelind <79406471+linelind@users.noreply.github.com>
2021-10-31 12:56:29 +0100

    added sort-button to shop page.

--- js/shop.js
+++ js/shop.js
@@ -7,9 +7,7 @@ async function fetchGames() {
         const response = await fetch(url);
         const json = await response.json();
 
-
         shopContainer.innerHTML = "";
-
 
         json.forEach(function(game) {
 
@@ -27,7 +25,7 @@ async function fetchGames() {
                                                     <div class="image">
                                                         <img src="${image[0].src}" alt="${image[0].alt}"/> 
                                                     </div>
-                                                    <div class="details">
+                                                    <div class="details">                                                        
                                                         <h2>${game.name}</h2>   
                                                         <p class="shopPrice">${game.price_html}</p> 
                                                         <p class="cta_orange cta_viewGame">View game</p>                                                                                                                                                     
@@ -39,25 +37,62 @@ async function fetchGames() {
                                                     <div class="image">
                                                         <img src="${image[0].src}" alt="${image[0].alt}"/> 
                                                     </div>
-                                                    <div class="details">
+                                                    <div class="details">                                                        
                                                         <h2>${game.name}</h2>   
                                                         <p class="shopPrice">${game.price_html}</p> 
                                                         <p class="cta_orange cta_viewGame">View game</p>                                                                                                                                                     
                                                     </div>
                                                 </a>`;
             }
-        
+
+            function sortContent(json){
+
+                shopContainer.innerHTML = "";
+                json.forEach(function(game){
+
+                    const image = game.images;
+
+                    for(let i = 0; i < image.length; i++) {
+                    }
+
+                    const onSale = game.on_sale;
+
+                    if(onSale === true){
+                        shopContainer.innerHTML += `<a href="productdetails.html?id=${game.id}" class="gameCard">
+                                                        <p class="saleLabel saleLabelShop">Sale</p>
+                                                        <div class="image">
+                                                            <img src="${image[0].src}" alt="${image[0].alt}"/> 
+                                                        </div>
+                                                        <div class="details">
+                                                            <h2>${game.name}</h2>   
+                                                            <p class="shopPrice">${game.price_html}</p> 
+                                                            <p class="cta_orange cta_viewGame">View game</p>                                                                                                                                                     
+                                                        </div>
+                                                    </a>`;            
+                    } else {
+                        shopContainer.innerHTML += `<a href="productdetails.html?id=${game.id}" class="gameCard">
+                                                        <div class="image">
+                                                            <img src="${image[0].src}" alt="${image[0].alt}"/> 
+                                                        </div>
+                                                        <div class="details">
+                                                            <h2>${game.name}</h2>   
+                                                            <p class="shopPrice">${game.price_html}</p> 
+                                                            <p class="cta_orange cta_viewGame">View game</p>                                                                                                                                                     
+                                                        </div>
+                                                    </a>`;
+                    }
+                })
+            }            
+            
+            document.querySelector(".sortAZ").addEventListener("click", function(){
+                json.sort((a,b) => (a.name > b.name) ? 1 : -1);
+                sortContent(json);
+            })
         }); 
-
-
     }
-
     catch(error) {
         shopContainer.innerHTML = createMessage("An error occured while calling the API.");
     }
-
 }
 
 fetchGames();
-
-
